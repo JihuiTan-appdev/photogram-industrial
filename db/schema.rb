4ca0312202f4b2e7ad8635418e60ad721ec4e169
JihuiTanUchicago <tanjihui@uchicago.edu>
2022-04-19 14:40:03 +0000

    pt1 database done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_174630) do
+ActiveRecord::Schema.define(version: 2022_04_19_142905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_174630) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.citext "email", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -44,5 +44,4 @@ ActiveRecord::Schema.define(version: 2021_04_16_174630) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "photos", "users", column: "owner_id"
 end
